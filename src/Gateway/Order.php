1d69dc055db4ac00f4c7d319828d370fd668ee21
Ricardo Ferreira <ricardoconstantino30@gmail.com>
2021-09-07 18:05:48 -0300

    refatoring orders

--- src/Gateway/Order.php
+++ src/Gateway/Order.php
@@ -9,45 +9,36 @@ class Order
 {
     private $configuration;
 
+    private $gateway;
+
     public function __construct()
     {
         $this->configuration = new Configuration();
+        $this->gateway = new GatewaySend();
     }
 
     public function getAllOrders()
     {
         $uri = $this->configuration->getBaseUrl()."v1/order";
-
-        $gateway = new GatewaySend();
-
-        return $gateway->execute('GET', $uri, "", $this->configuration::TOKEN);
+        return $this->gateway->execute('GET', $uri, "", $this->configuration::TOKEN);
     }
 
     public function getOrderByTransaction($transaction)
     {
         $uri = $this->configuration->getBaseUrl()."v1/order/".$transaction;
-
-        $gateway = new GatewaySend();
-
-        return $gateway->execute('GET', $uri, "", $this->configuration::TOKEN);
+        return $this->gateway->execute('GET', $uri, "", $this->configuration::TOKEN);
     }
 
     public function getOrderByOrderRef($orderRef)
     {
         $uri = $this->configuration->getBaseUrl()."v1/order?order_ref=".$orderRef;
-
-        $gateway = new GatewaySend();
-
-        return $gateway->execute('GET', $uri, "", $this->configuration::TOKEN);
+        return $this->gateway->execute('GET', $uri, "", $this->configuration::TOKEN);
     }
 
     public function save($modelOrder)
     {
         $uri = $this->configuration->getBaseUrl()."v1/order";
         $body = $modelOrder->getEncodeParams();
-
-        $gateway = new GatewaySend();
-
-        return $gateway->execute('POST', $uri, $body, $this->configuration::TOKEN);
+        return $this->gateway->execute('POST', $uri, $body, $this->configuration::TOKEN);
     }
 }
